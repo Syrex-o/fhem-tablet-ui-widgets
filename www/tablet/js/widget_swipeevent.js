ccd63ec8e581041cb35d95290aae5095e448056c
Syrex-o <alberthenkel.ah@gmail.com>
2018-04-16 20:34:26 +0200

    Update widget_swipeevent.js
    added directional methods for left - right swiping

--- www/tablet/js/widget_swipeevent.js
+++ www/tablet/js/widget_swipeevent.js
@@ -12,58 +12,92 @@ function depends_swipeevent(){
 }
 
 var Modul_swipeevent = function(){
-	var startX
-	var startY
-	var endX
-	var endY
-	var treshold = 250;
+	var startX, startY, endX, endY;
 	function handleTouch(elem, reactor, change, startX, endX, startY, endY, cbL, cbR, cbU, cbD, target){
 	  	var xDist = endX - startX;
 	  	var yDist = endY - startY;
 	  	if(endX - startX < 0 && xDist < -elem.data("swipeX")){
-	  	   cbL(target, reactor, change);
+	  	   cbL(target, reactor, change, elem);
 	  	}else if(endX - startX > 0 && xDist > elem.data("swipeX")){
-	  	   cbR(target, reactor, change);
+	  	   cbR(target, reactor, change, elem);
 	    }
 	    if(endY - startY < 0 && yDist < -elem.data("swipeY")){
-	    	cbU(target, reactor, change);
-	    }else{
-	    	cbD(target, reactor, change);
+	    	cbU(target, reactor, change, elem);
+	    }else if(endY - startY > 0 && yDist > elem.data("swipeY")){
+	    	cbD(target, reactor, change, elem);
 	    }
 	}
-	var left = function(target, reactor, change){
-		if($(""+ reactor +"").hasClass(change)){
-			target.trigger("click");
+	function validator(elem, x){
+		if(x === true){
+			if (elem.find("data-opendirX").context.dataset.opendirx){
+		  		return elem.find("data-opendirX").context.dataset.opendirx;
+		  	}
+		  	else{
+		  		return elem.data("opendirX");
+		 	}
 		}
 		else{
-			return;
+			if (elem.find("data-opendirY").context.dataset.opendirx){
+		  		return elem.find("data-opendirY").context.dataset.opendirx;
+		  	}
+		  	else{
+		  		return elem.data("opendirY");
+		 	}
 		}
 	}
-	var right = function(target, reactor, change){
-		if($(""+ reactor +"").hasClass(change)){
-			return;
+	var left = function(target, reactor, change, elem){
+		if (validator(elem, true) === "right"){
+			if($(""+ reactor +"").hasClass(change)){
+				target.trigger("click");
+			}
+			else{
+				return;
+			}
 		}
 		else{
-			target.trigger("click");
+			if($(""+ reactor +"").hasClass(change)){
+				return;
+			}
+			else{
+				target.trigger("click");
+			}
 		}
 	}
-	var up = function(target, reactor, change){
+	var right = function(target, reactor, change, elem){
+		if(validator(elem, true) === "right"){
+			if($(""+ reactor +"").hasClass(change)){
+				return;
+			}
+			else{
+				target.trigger("click");
+			}
+		}
+		else{
+			if($(""+ reactor +"").hasClass(change)){
+				target.trigger("click");
+			}
+			else{
+				return;
+			}
+		}
 	}
-	var down = function(target, reactor, change){
+	var up = function(target, reactor, change, elem){
 	}
-
+	var down = function(target, reactor, change, elem){
+	}
 	function init_attr(elem){
 		elem.initData("device", "");
 		elem.initData("get", "STATE");
 		elem.initData("reactor", 'html');
 		elem.initData("change", "slideout-open");
 		elem.initData('target', '#slideout');
+		elem.initData('opendirX', "right");
+		elem.initData('opendirY', "up");
 		elem.initData("swipeX", $(window).innerWidth()/2.5);
 		elem.initData("swipeY", $(window).innerHeight()/4);
 		elem.css({"display": "none"});
 
 	 	me.addReading(elem, 'get');
-	 	me.addReading(elem, "target");
 
 	 	window.addEventListener('touchstart', function(event){
 	    	startX = event.touches[0].clientX;
@@ -78,7 +112,6 @@ var Modul_swipeevent = function(){
 	   		handleTouch(elem, reactor, change, startX, endX, startY, endY, left, right, up, down, target);
 	 	});
 	}
-
 	function update(dev, par) {
 		me.elements.filterDeviceReading('get', dev, par)
             .each(function (index) {
